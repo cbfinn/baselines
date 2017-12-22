a3820effde833d0dc738028828af96878c14cfcc
Chelsea Finn <cbfinn@eecs.berkeley.edu>
2017-12-22 09:02:13 -0800

    run_mujoco.py

--- baselines/maml_ppo/run_mujoco.py
+++ baselines/maml_ppo/run_mujoco.py
@@ -30,7 +30,7 @@ def train(env_id, num_timesteps, seed):
     ray.init()
     maml_ppo.learn(policy=policy, get_env=get_env, nsteps=2048, nminibatches=64,
         lam=0.95, gamma=0.99, noptepochs=10, log_interval=1, ent_coef=0.0,
-        lr=3e-4, inner_lr=0.1,
+        lr=3e-4, inner_lr=0.01,
         cliprange=0.2,
         ntasks=4,
         total_timesteps=num_timesteps)
